b335bab50610b55879e5961547cff870c4060b73
garylzimmer <garylzimmer@gmail.com>
2016-06-23 16:37:53 -0400

    Implemented BuyFirstItem and HideRowIfZero, TODO: ReturnItem method

--- Magic Shop/Form1.cs
+++ Magic Shop/Form1.cs
@@ -111,6 +111,23 @@ namespace Magic_Shop
             }
             return clonedRow;
         }
+        
+        public void BuyFirstItem(int storeItemRowNum)
+        {
+        	DataGridViewRow tempRow = (DataGridViewRow) CloneWithValues(storeGridView.Row(storeItemRowNum));
+        	scGridView.AddRow();
+        	scGridView.row[scGridView.RowCount - 1] = tempRow;
+        	scGridView.row[scGridView.RowCount - 1].Cell["scQuantity"].Value = 1;
+        	return;
+        }
+        
+        public void HideRowIfZero(int itemRowNum)
+        {
+        	if(storeGridView.Rows[itemRowNum].Cells["storeQuantityCol"] <= 0)
+        	{
+        		storeGridView.Rows[itemRowNum].Cells["storeQuantityCol"].Visible = False;
+        	}
+        }
 
 
         private void BuyItemFromStore(int storeItemRowNum)
@@ -127,8 +144,8 @@ namespace Magic_Shop
            
             if(scGridView.Rows[0]==null) //if shopping cart is empty
             {
-            	//BuyFirstItem(storeItemRowNum);
-            	//HideRowIfZero(storeItemRowNum);
+            	BuyFirstItem(storeItemRowNum);
+            	HideRowIfZero(storeItemRowNum);
             	return;
             }
             else //shopping cart not empty
@@ -140,70 +157,59 @@ namespace Magic_Shop
             	{
             		if(storeItemName.Equals(scGridView.Rows[i].Cells["scItemNameCol"].Value)
             		   {
+            		   	//use var to find the index of the possible dupe
             		   	cartItemDuplicateIndex = i;
             		   }
             	}
             	if(cartItemDuplicateIndex > -1) //cart not empty, item is a dupe
             	{
             		   	//add 1 to the qty of cartItemDuplicateIndex's qty
-            		   	//HideRowIfZero(storeItemRowNum);
+            		   	HideRowIfZero(storeItemRowNum);
             		   	return;
             	}
 	            //shopping cart not empty but did not find duplicate
 	            
-	            		//BuyFirstItem(storeItemRowNum);
-	            		//HideRowIfZero(storeItemRow);
+	            		BuyFirstItem(storeItemRowNum);
+	            		HideRowIfZero(storeItemRow);
 	            		return;
             }
 
         }
-        private void ReturnItemToStore(string scItemName)
+        private void ReturnItemToStore(int scItemRowNum)
         {
-            //iterate through the store's rows
-            for (int storeRow = 0; storeRow<storeGridView.Rows.Count; storeRow++)
-            {
-                //store the iterated row's item name
-                string searchedStoreItemName = (string) storeGridView.Rows[storeRow].Cells["storeItemNameCol"].Value;
-                if(scItemName.Equals(searchedStoreItemName))
-                {
-                    System.Windows.Forms.MessageBox.Show("Found that: Shopping Cart's " + scItemName + " matches Store's " + searchedStoreItemName);
-                    //get row for the found store item based on iterated row and item name col
-                    int foundStoreItemRowIndex = storeGridView.Rows[storeRow].Cells["storeItemNameCol"].RowIndex;
-                    
-                    //get int qty of found store item based on above and quantity col
-                    int foundStoreItemQty = (int)storeGridView.Rows[foundStoreItemRowIndex].Cells["storeQuantityCol"].Value;
-                    //increment store item qty value
-                    foundStoreItemQty++;
-                    //update store item qty value
-                    storeGridView.Rows[foundStoreItemRowIndex].Cells["storeQuantityCol"].Value = foundStoreItemQty;
-                    storeGridView.Rows[storeRow].Visible = true;
-
-                    //find row of the sc item
-                    for (int scRow = 0; scRow < scGridView.Rows.Count; scRow++)
-                    {
-                        if (scItemName.Equals(scGridView.Rows[scRow].Cells["scItemNameCol"].Value))
-                        {
-                            //from scRow, get the quantity of the item
-                            int scItemQty = (int)scGridView.Rows[scRow].Cells["scQuantityCol"].Value;
-                            //decrement qty
-                            scItemQty--;
-                            //update qty in cell
-                            scGridView.Rows[scRow].Cells["scQuantityCol"].Value = scItemQty;
-
-                            //hide scRow if qty <=0
-                            if(scItemQty<=0)
-                            {
-                                scGridView.CurrentCell = null;
-                                scGridView.Rows[scRow].Visible = false;
-                            }
-
-                            
-                        }
-                    }
-                }
-
-            }
-
+        	//find the name of the item in the store's item row
+        	string scItemName = (string)scGridView.Rows[scItemRowNum].Cells["scItemNameCol"].Value;
+        	//find qty of of the item in the store's item row
+        	int scItemQty = (int)scGridView.Rows[scItemRowNum].Cells["scQuantityCol"].Value;
+        	//decrement qty
+        	scItemQty--;
+        	//update qty
+        	scGridView.Rows[scItemRowNum].Cells["scQuantityCol"].Value = scItemQty;
+       		//HideRowIfZero(scItemRowNum);
+        	
+        	//initialize dupe check index
+        	int storeDupeIndex = -1;
+        	//iterate through the store to look for duplicates
+        	for(int i = 0; i<storeGridView.Rows.Count; i++)
+        	{
+        		if(scItemName.Equals(storeGridView.Rows[i].Cells["storeItemNameCol"].Value))
+        		{
+        			storeDupeIndex = i;
+        		}
+        	}
+        	
+        	if(storeDupeIndex > -1)
+        	{
+        		//add 1 to the qty of storeDupeIndex's qty
+        		int storeDupeQty = storeGridView.Rows[storeDupeIndex].Cells["storeQuantity"].Value;
+        		storeDupeQty++;
+        		storeGridView.Rows[storeDupeIndex].Cells["storeQuantity"].Value = storeDupeQty;
+        		storeGridView.Rows[storeDupeIndex].Visible = True;
+        		return;
+        	}
+        	else //returned item wasn't found in the store...
+        		//how'd you get here?
+        		return;
         }
         //clicking on store button or item name for descript method
         private void storeGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
